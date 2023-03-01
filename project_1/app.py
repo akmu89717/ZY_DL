c4d8e0bb382d8193dc705c037b2ab989018785a1
akmu89717 <125662493+akmu89717@users.noreply.github.com>
2023-03-01 16:00:39 +0800

    final vision

--- project_1/app.py
+++ project_1/app.py
@@ -14,7 +14,7 @@ RED = (255,0,0)
 
 IMAGESAVE = False 
 
-MODEL = load_model("bestmodel.h5")
+MODEL = load_model("temp/bestmodel.h5")
 
 LABELS = {0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Sevsen",8:"Eight",9:"Nine"}
 
@@ -54,14 +54,16 @@ while True:
             iswriting  = False
             number_xcord = sorted(number_xcord)
             number_ycord = sorted(number_ycord)
-
-            rect_min_x , rect_max_x =max(number_xcord[0]- BOUNDARY ,0), min(WINDOWSIZEX ,number_xcord[-1]+BOUNDARY)
-            rect_min_y , rect_max_y =max(number_ycord[0]- BOUNDARY), min(WINDOWSIZEY ,number_ycord[-1]+BOUNDARY)
+            # print('erfwera',number_xcord[0]- BOUNDARY)
+            rect_min_x =max(number_xcord[0]- BOUNDARY ,0)
+            rect_max_x=min(WINDOWSIZEX ,number_xcord[-1]+BOUNDARY)
+            # print('sgg',number_ycord[0]- BOUNDARY,number_ycord[-1]+BOUNDARY,WINDOWSIZEY)
+            rect_min_y , rect_max_y =max(number_ycord[0]- BOUNDARY,0), min(WINDOWSIZEY ,number_ycord[-1]+BOUNDARY)
 
             number_xcord =[]
             number_ycord = []
 
-            ing_arr = np.array(pygame.PixelArray(DISPLAYSURF))[rect_min_x:rect_max_x,rect_min_y:rect_max_y].T.astype(np.float32)
+            img_arr = np.array(pygame.PixelArray(DISPLAYSURF))[rect_min_x:rect_max_x,rect_min_y:rect_max_y].T.astype(np.float32)
 
             if IMAGESAVE:
                 cv2.imwrite("image.png")
@@ -70,21 +72,22 @@ while True:
             if PREDICT:
                 image = cv2.resize(img_arr,(28,28))
                 image = np.pad(image,(10,10),'constant',constant_values=0)
-                image = cv2.resize(imag,(28,28))/255
+                image = cv2.resize(image,(28,28))/255
 
                 label = str(LABELS[np.argmax(MODEL.predict(image.reshape(1,28,28,1)))])
-
-                textSurface =- FONT.render(label,True,RED, WHITE)
-                textRecObj = testing.get_rect()
-                textRecObj.left , textRecObj.bottom = rect_max_x,rect_max_y
-
-                DISPLAYSURF.blit(textSurface,textRecObj)
+                print(label)
+                FONT= pygame.font.SysFont('Comic Sans MS', 30)
+                textSurface = FONT.render(label,True,RED, WHITE)
+                # textRecObj =image.png.get_rect()
+                # textRecObj.left , textRecObj.bottom = rect_max_x,rect_max_y
+                # DISPLAYSURF.blit(textSurface,textRecObj)
+                DISPLAYSURF.blit(textSurface,(0,0))
 
             if event.type == KEYDOWN:
                 if event.unicode =="n":
                     DISPLAYSURF.fill(BLACK)
 
-        pygame.display
+    pygame.display.update()
 
 
 
